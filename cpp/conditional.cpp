8dcdd973266f89f29e254b727a75653a2bf97ac1
EshantBagwe <eshantbagwe2000@gmail.com>
2023-12-17 12:51:56 +0530

    condtionals prblm

---
+++ cpp/conditional.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+// q1 smaller no between 2
+//    int a;
+//     cout<<"Enter a:";
+//     cin>>a;
+//     int b;
+//     cout<<"Enter b:";
+//     cin>>b;
+//     if(a<=b){
+//         cout<<a<<" is smaller no";
+//         if(a>=b){
+//         cout<<b<<" is smaller no";
+//     }
+//     }
+
+// q2 leap year
+
+// int year;
+// cout<<"Enter year : ";
+// cin>>year;
+// if(year%4==0){
+//     if(year%40==0){
+//         if(year%400==0){
+//             cout<<year<<"is a leap year";
+//         }else{
+//             cout<<year<<" is not a leap year";
+//         }
+//     }else{
+//             cout<<year<<" is not a leap year";
+//     }
+// }else{
+//             cout<<year<<" is not a leap year";
+// }
+
+// write a code to swap 2 no without using 3 value
+// int a,b;
+// cout<<"Enter a:";
+// cin>>a;
+// cout<<"Enter b:";
+// cin>>b;
+// a = a+b;
+// b = a-b;
+// a = a-b;
+// cout<<"A:"<<a<<endl;
+// cout<<"B:"<<b;
+
+// value even or odd
+
+// int a;
+// cout<<"enter value: ";
+// cin>>a;
+// int num = a%2;
+// switch(num){
+//     case 0:
+//     cout<<a<<"is even"<<endl;
+//     break;
+//     case 1:
+//     cout<<a<<"is odd";
+// }
+
+// q5 student grade
+// int marks;
+// char grade;
+// cout<<"Enter your marks here: ";
+// cin>>marks;
+// if(marks>=80){
+//     grade='A';
+//     cout<<"you got "<<grade<<" grade";    
+// }else if(marks>=70 && marks<80){
+//     grade='B';
+//    cout<<"you got "<<grade<<" grade";
+// }else if(marks>=60 && marks<70){
+//     grade='C';
+//     cout<<"you got "<<grade<<" grade";
+// }else if(marks>=50 && marks<60){
+//     grade='D';
+//    cout<<"you got "<<grade<<" grade";
+// }else if(marks>=35 && marks<50){
+//     grade='E';
+//     cout<<"you got "<<grade<<" grade";
+// }else if(marks<35){
+//     grade='F';
+//     cout<<"you got "<<grade<<" grade";
+// }
+    return 0;
+}
